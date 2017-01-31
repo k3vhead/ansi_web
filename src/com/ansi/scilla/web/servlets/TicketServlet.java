7b4209e3d32231bb99d1e06280935cb1b99b88c2
Gary <gag@ansi.com>
2017-01-31 14:33:31 -0600

    Added some commented out doPost() code for transfer to home computer.

--- src/com/ansi/scilla/web/servlets/TicketServlet.java
+++ src/com/ansi/scilla/web/servlets/TicketServlet.java
@@ -11,7 +11,10 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.ansi.scilla.web.common.AppUtils;
 import com.ansi.scilla.web.common.ResponseCode;
+import com.ansi.scilla.web.request.DivisionRequest;
 import com.ansi.scilla.web.response.ticket.TicketReturnListResponse;
+import com.ansi.scilla.web.struts.SessionUser;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.thewebthing.commons.db2.RecordNotFoundException;
 
 
@@ -23,16 +26,17 @@ import com.thewebthing.commons.db2.RecordNotFoundException;
  * 					
  * 
  * The url for update will be a POST to:
- * 		/ticket/<ticket>/return/<action> with parameters in the JSON
- * 			Action 		Parameters
- * 			complete	<processDate><processNotes><actualPricePerCleaning>
- * 						<actualDirectLaborPct><actualDirectLabor>
- * 						<customerSignature><billSheet><managerApproval>
- * 			skip		<processDate><processNotes>
- * 			void		<processDate><processNotes>
- * 			reject		<processDate><processNotes>
- * 			re-queue	-none-
+ * 		/ticket/<ticket>/return with parameters in the JSON
+ * 			Action 		Next Status		Parameters
+ * 			complete	"C"				<nextStatus><processDate><processNotes><actualPricePerCleaning>
+ * 										<actualDirectLaborPct><actualDirectLabor>
+ * 										<customerSignature><billSheet><managerApproval>
+ * 			skip		"S"				<nextStatus><processDate><processNotes>
+ * 			void		"V"				<nextStatus><processDate><processNotes>
+ * 			reject		"R"				<nextStatus><processDate><processNotes>
+ * 			re-queue	"G"				<nextStatus>
  * 
+ * 		/ticket/<ticket>/invoice returns 405? as the UI has no functionality for the user to update the invoice panel
  * 
  * @author ggroce
  */
@@ -41,6 +45,7 @@ public class TicketServlet extends AbstractServlet {
 	private static final long serialVersionUID = 1L;
 	private String panel;
 	private Integer id;
+	private String remainder;
 	
 	/**
 	 * Parses a URL of the form /<servlet>/<id>/<panel>
@@ -103,6 +108,30 @@ public class TicketServlet extends AbstractServlet {
 					AppUtils.closeQuiet(conn);
 				}
 			} else if ( this.panel.equals("invoice")) { // ticket invoice panel?
+				/* Needs to return:
+				 * 		for Ticket = ticketId
+				 * 			act_price_per_cleaning
+				 * 			act_tax_amt
+				 * 			sum(ticket_payment.amount)
+				 * 			sum(ticket_payment.tax_amt)
+				 * 			balance(total of ppc + tax - (payment amt + payment tax))
+				 * 			daysToPay(today, invoiceDate, balance) 
+				 * 					if balance == 0, daysToPay = max(paymentDate)-invoiceDate
+				 * 					if balance <> 0, daysToPay = today - invoiceDate
+				 * 			**ticket write off amount - stub for v 2.0
+
+				 * 		for Invoice = invoiceId
+				 * 			invoice_id (this is the invoice number)
+				 * 			sum(act_price_per_cleaning)
+				 * 			sum(act_tax_amt)
+				 * 			sum(ticket_payment.amount)
+				 * 			sum(ticket_payment.tax_amt)
+				 * 			balance(total of inv.ppc + inv.tax - (inv.payment amt + inv.payment tax))
+				 * 			**invoice write off amount - stub for v 2.0
+				 * 			**invoice MSFC amount - stub for v 2.0
+				 * 			**invoice excess payment amount - stub for v 2.0
+				 * 
+				 */
 				System.out.println("Ticket(): doGet(): process ticket invoice panel");
 				super.sendNotFound(response); // not coded yet
 			} else {
@@ -114,7 +143,7 @@ public class TicketServlet extends AbstractServlet {
 	}
 
 /*	@Override
-	protected void doGetOriginal(HttpServletRequest request,
+	protected void doGet(HttpServletRequest request,   // before I pulled out the parsePanelUrl() method
 			HttpServletResponse response) throws ServletException, IOException {
 		String url = request.getRequestURI();
 		System.out.println("TicketReturn(): doGet(): Url:" + url);
@@ -173,152 +202,91 @@ public class TicketServlet extends AbstractServlet {
 */
 
 /*	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		SessionUser sessionUser = AppUtils.getSessionUser(request);
 		String url = request.getRequestURI();
-//		String queryString = request.getQueryString();
-		System.out.println("TaxRateServlet: doPost() Url:" + url);
-		
-		Connection conn = null;
-		try {
-			conn = AppUtils.getDBCPConn();
-			conn.setAutoCommit(false);
-
-			// figure out if this is an "add" or an "update"
-			int idx = url.indexOf("/taxRate/");
-			String myString = url.substring(idx + "/taxRate/".length());				
-			String[] urlPieces = myString.split("/");
-			String command = urlPieces[0];
-			System.out.println("TaxRateServlet: doPost() command:"+command);
-
-			String jsonString = super.makeJsonString(request);
-			System.out.println("TaxRateServlet: doPost() jsonString:"+jsonString);
-			TaxRateRequest taxRateRequest = new TaxRateRequest(jsonString);
-			
-			TaxRate taxRate = null;
-			ResponseCode responseCode = null;
-			if ( command.equals(ACTION_IS_ADD) ) {
-				System.out.println("TaxRateServlet: doPost() action is add");
-				WebMessages webMessages = validateAdd(conn, taxRateRequest);
-				if (webMessages.isEmpty()) {
-					try {
-						taxRate = doAdd(conn, taxRateRequest, sessionUser);
-						String message = AppUtils.getMessageText(conn, MessageKey.SUCCESS, "Success!");
-						responseCode = ResponseCode.SUCCESS;
-						webMessages.addMessage(WebMessages.GLOBAL_MESSAGE, message);
-					} catch ( DuplicateEntryException e ) {
-						String messageText = AppUtils.getMessageText(conn, MessageKey.DUPLICATE_ENTRY, "Record already Exists");
-						webMessages.addMessage(WebMessages.GLOBAL_MESSAGE, messageText);
-						responseCode = ResponseCode.EDIT_FAILURE;
-					} catch ( Exception e ) {
-						responseCode = ResponseCode.SYSTEM_FAILURE;
-						AppUtils.logException(e);
-						String messageText = AppUtils.getMessageText(conn, MessageKey.INSERT_FAILED, "Insert Failed");
-						webMessages.addMessage(WebMessages.GLOBAL_MESSAGE, messageText);
-					}
-				} else {
-					System.out.println("TaxRateServlet: doPost() add failed");
-					responseCode = ResponseCode.EDIT_FAILURE;
+		System.out.println("TicketReturn(): doPost(): Url:" + url);
+		if(parsePanelUrl("/ticket/",url)) {
+			System.out.println("Ticket(): doPost(): panel:" + this.panel);
+			System.out.println("Ticket(): doPost(): ticketId:" + this.id);
+			if ( this.panel.equals("return")) { // ticket return panel?
+				System.out.println("Ticket(): doPost(): process ticket return panel");
+				String jsonString = super.makeJsonString(request);
+				try {
+					TicketReturnRequest ticketReturnRequest = (TicketReturnRequest) AppUtils.json2object(jsonString, TicketReturnRequest.class);
+					processTicketReturnPostRequest(conn, response, command, sessionUser, ticketReturnRequest);
+				} catch ( InvalidFormatException formatException) {
+					processBadPostRequest(conn, response, formatException);
 				}
-				System.out.println("TaxRateServlet: doPost() prepare response");
-				TaxRateResponse taxRateResponse = new TaxRateResponse(taxRate, webMessages);
-				System.out.println("TaxRateServlet: doPost() send response");
-				super.sendResponse(conn, response, responseCode, taxRateResponse);
-				System.out.println("TaxRateServlet: doPost() response sent");
+
+				SessionUser sessionUser = AppUtils.getSessionUser(request);
+				String url = request.getRequestURI();
 				
-			} else if ( urlPieces.length == 1 ) {   //  /<taxRateId> = 1 pieces
-				System.out.println("TaxRateServlet: doPost() action is update");
-				WebMessages webMessages = validateAdd(conn, taxRateRequest);
-				if (webMessages.isEmpty()) {
-					System.out.println("passed validation");
+===============
+				Connection conn = null;
+				try {
+					conn = AppUtils.getDBCPConn();
+					conn.setAutoCommit(false);
+
+					// figure out if this is an "add" or an "update"
+					int idx = url.indexOf("/division/");
+					String myString = url.substring(idx + "/division/".length());		
+					String[] urlPieces = myString.split("/");
+					String command = urlPieces[0];
+					String jsonString = super.makeJsonString(request);
 					try {
-						TaxRate key = new TaxRate();
-						if ( StringUtils.isNumeric(urlPieces[0]) ) {//looks like a taxRateId
-							System.out.println("TaxRateServlet: doPost() trying to update:"+urlPieces[0]);
-							key.setTaxRateId(Integer.valueOf(urlPieces[0]));
-							taxRate = doUpdate(conn, key, taxRateRequest, sessionUser);
-							String message = AppUtils.getMessageText(conn, MessageKey.SUCCESS, "Success!");
-							responseCode = ResponseCode.SUCCESS;
-							webMessages.addMessage(WebMessages.GLOBAL_MESSAGE, message);
-						} else { //non-integer taxRateId, probably a bad thing
-							throw new RecordNotFoundException();
-						}
-					} catch ( RecordNotFoundException e ) {
-						System.out.println("Doing 404");
-						super.sendNotFound(response);						
-					} catch ( Exception e) {
-						System.out.println("Doing SysFailure");
-						responseCode = ResponseCode.SYSTEM_FAILURE;
-						AppUtils.logException(e);
-						String messageText = AppUtils.getMessageText(conn, MessageKey.INSERT_FAILED, "Insert Failed");
-						webMessages.addMessage(WebMessages.GLOBAL_MESSAGE, messageText);
+						DivisionRequest divisionRequest = (DivisionRequest) AppUtils.json2object(jsonString, DivisionRequest.class);
+						processPostRequest(conn, response, command, sessionUser, divisionRequest);
+					} catch ( InvalidFormatException formatException) {
+						processBadPostRequest(conn, response, formatException);
 					}
-				} else {
-					System.out.println("Doing Edit Fail");
-					responseCode = ResponseCode.EDIT_FAILURE;
+				} catch ( Exception e ) {
+					AppUtils.logException(e);
+					AppUtils.rollbackQuiet(conn);
+					throw new ServletException(e);
+				} finally {
+					AppUtils.closeQuiet(conn);
 				}
-				System.out.println("TaxRateServlet: doPost() prepare response");
-				TaxRateResponse taxRateResponse = new TaxRateResponse(taxRate, webMessages);
-				System.out.println("TaxRateServlet: doPost() send response");
-				super.sendResponse(conn, response, responseCode, taxRateResponse);
-				System.out.println("TaxRateServlet: doPost() response sent");
+				
+				
+
+			} else if ( this.panel.equals("invoice")) { // ticket invoice panel?
+*/				/* Needs to return:
+				 * 		for Ticket = ticketId
+				 * 			act_price_per_cleaning
+				 * 			act_tax_amt
+				 * 			sum(ticket_payment.amount)
+				 * 			sum(ticket_payment.tax_amt)
+				 * 			balance(total of ppc + tax - (payment amt + payment tax))
+				 * 			daysToPay(today, invoiceDate, balance) 
+				 * 					if balance == 0, daysToPay = max(paymentDate)-invoiceDate
+				 * 					if balance <> 0, daysToPay = today - invoiceDate
+				 * 			**ticket write off amount - stub for v 2.0
+
+				 * 		for Invoice = invoiceId
+				 * 			invoice_id (this is the invoice number)
+				 * 			sum(act_price_per_cleaning)
+				 * 			sum(act_tax_amt)
+				 * 			sum(ticket_payment.amount)
+				 * 			sum(ticket_payment.tax_amt)
+				 * 			balance(total of inv.ppc + inv.tax - (inv.payment amt + inv.payment tax))
+				 * 			**invoice write off amount - stub for v 2.0
+				 * 			**invoice MSFC amount - stub for v 2.0
+				 * 			**invoice excess payment amount - stub for v 2.0
+				 * 
+				 */
+/*				System.out.println("Ticket(): doGet(): process ticket invoice panel");
+				super.sendNotFound(response); // not coded yet
 			} else {
-				super.sendNotFound(response);
+				super.sendNotFound(response); // unexpected panel
 			}
-			
-			conn.commit();
-		} catch ( Exception e ) {
-			AppUtils.logException(e);
-			AppUtils.rollbackQuiet(conn);
-			throw new ServletException(e);
-		} finally {
-			AppUtils.closeQuiet(conn);
+		} else {
+			super.sendNotFound(response);
 		}
-		
-	}
-
-
-	protected TaxRate doUpdate(Connection conn, TaxRate key, TaxRateRequest taxRateRequest, SessionUser sessionUser) throws Exception {
-		System.out.println("This is the key:");
-		System.out.println(key);
-		System.out.println("************");
-		Date today = new Date();
-		TaxRate taxRate = new TaxRate();
-		taxRate.setAmount(taxRateRequest.getAmount());
-		taxRate.setEffectiveDate(taxRateRequest.getEffectiveDate());
-		if ( ! StringUtils.isBlank(taxRateRequest.getLocation())) {
-			taxRate.setLocation(taxRateRequest.getLocation());
-		}
-		taxRate.setRate(taxRateRequest.getRate());
-		//taxRate.setTaxRateId(taxRateRequest.getTaxRateId()); //key is passed in from the url
-		taxRate.setUpdatedBy(sessionUser.getUserId());
-		taxRate.setUpdatedDate(today);
-		// if we update something that isn't there, a RecordNotFoundException gets thrown
-		// that exception get propagated and turned into a 404
-		taxRate.update(conn, key);		
-		return taxRate;
-	}
-
-	protected WebMessages validateUpdate(Connection conn, TaxRate key, TaxRateRequest taxRateRequest) throws RecordNotFoundException, Exception {
-		WebMessages webMessages = new WebMessages();
-		System.out.println("TaxRateServlet: validateUpdate() before");
-		List<String> missingFields = super.validateRequiredUpdateFields(taxRateRequest);
-		System.out.println("TaxRateServlet: validateUpdate() after");
-		if ( ! missingFields.isEmpty() ) {
-			System.out.println("TaxRateServlet: validateUpdate() missing fields");
-			String messageText = AppUtils.getMessageText(conn, MessageKey.MISSING_DATA, "Required Entry");
-			for ( String field : missingFields ) {
-				webMessages.addMessage(field, messageText);
-			}
-		}
-		// if we "select" the key, and it isn't found, a "RecordNotFoundException" is thrown.
-		// That exception will propagate up the tree until it turns into a 404 message sent to the client
-		TaxRate testKey = (TaxRate)key.clone(); 
-		System.out.println("TaxRateServlet: validateUpdate() testKey:" + testKey.getTaxRateId());
-		testKey.selectOne(conn);
-		return webMessages;
 	}
 */
+
+
 	
 }
