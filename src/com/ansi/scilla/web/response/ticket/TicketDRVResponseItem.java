582f2cfbe415429f03bb6485a4d01a2945e9e312
DC Lewis <dclewis@thewebthing.com>
2017-02-02 21:31:07 -0600

    Ticket DRV Servlet records

--- src/com/ansi/scilla/web/response/ticket/TicketDRVResponseItem.java
+++ src/com/ansi/scilla/web/response/ticket/TicketDRVResponseItem.java
@@ -4,9 +4,12 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 import com.ansi.scilla.common.ApplicationObject;
+import com.ansi.scilla.common.queries.TicketDRVQuery;
 
 public class TicketDRVResponseItem extends ApplicationObject {
 
+	private static final long serialVersionUID = 1L;
+	
 	private Integer ticketId;
 	private String status;
 	private String address1;
@@ -19,5 +22,101 @@ public class TicketDRVResponseItem extends ApplicationObject {
 	private BigDecimal budget;
 	private BigDecimal ppc;
 	private String cod;
+	
+	public TicketDRVResponseItem() {
+		super();
+	}
+	
+	public TicketDRVResponseItem(TicketDRVQuery record) {
+		this();
+		this.ticketId = record.getTicketId();
+		this.status = record.getStatus();
+		this.address1 = record.getAddress1();
+		this.budget = record.getBudget();
+		this.city = record.getCity();
+		this.cod = record.getInvoiceTerms();
+		this.frequency = record.getJobFrequency();
+		this.jobNum = record.getJobNbr();
+		this.lastDone = record.getLastDone();
+		this.name = record.getName();
+		this.ppc = record.getPricerPerCleaning();
+		this.startDate = record.getStartDate();
+	}
+	
+	public Integer getTicketId() {
+		return ticketId;
+	}
+	public void setTicketId(Integer ticketId) {
+		this.ticketId = ticketId;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getAddress1() {
+		return address1;
+	}
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public Date getLastDone() {
+		return lastDone;
+	}
+	public void setLastDone(Date lastDone) {
+		this.lastDone = lastDone;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Integer getJobNum() {
+		return jobNum;
+	}
+	public void setJobNum(Integer jobNum) {
+		this.jobNum = jobNum;
+	}
+	public String getFrequency() {
+		return frequency;
+	}
+	public void setFrequency(String frequency) {
+		this.frequency = frequency;
+	}
+	public BigDecimal getBudget() {
+		return budget;
+	}
+	public void setBudget(BigDecimal budget) {
+		this.budget = budget;
+	}
+	public BigDecimal getPpc() {
+		return ppc;
+	}
+	public void setPpc(BigDecimal ppc) {
+		this.ppc = ppc;
+	}
+	public String getCod() {
+		return cod;
+	}
+	public void setCod(String cod) {
+		this.cod = cod;
+	}
+	
+
+	
 	
 }
