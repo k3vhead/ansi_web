e758a118f006ba294374cec102463726f133bdb9
DC Lewis <dclewis@thewebthing.com>
2017-02-04 06:50:42 -0600

    Documentation update

--- src/com/ansi/scilla/web/common/AnsiURL.java
+++ src/com/ansi/scilla/web/common/AnsiURL.java
@@ -11,6 +11,19 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.ansi.scilla.web.exceptions.ResourceNotFoundException;
 
+/**
+ * Parses a "standard" url following the pattern:<br>
+ * /installation/realm/command/filter1/filter2?parm1=value1&amp;parm2=value2<br>
+ * where installation is determined by the deployment paradigm, "realm" is the servlet we're working with 
+ * (like "division" or "ticket"). The command will either be a numeric database keyfield or a
+ * command like "list" or "add". Filters are optional, and are servlet-specific. Query String
+ * parameters are also optional and servlet-specific. Both are parsed, but not validated.
+ * 
+ * Note that posted values (like the JSON in an "ADD") must be processed before invoking the constructor.
+ * 
+ * @author dclewis
+ *
+ */
 public class AnsiURL extends ApplicationWebObject {
 
 	private static final long serialVersionUID = 1L;
@@ -21,7 +34,16 @@ public class AnsiURL extends ApplicationWebObject {
 	private List<String> filterList;
 	private Map<String, String[]> queryParameterMap;
 	
-	
+	/**
+	 * Parse the URI from the given request.
+	 * Note that posted values (like the JSON in and "ADD") must be processed before invoking this constructor
+	 * 
+	 * @param request The unchanged HTTP request object, after posted values are processed 
+	 * @param expectedRealm Typically the servlet name, eg "ticket" or "division"
+	 * @param expectedCommandList A list of valid commands like "list" or "add". A null value means no commands are accepted
+	 * @throws ResourceNotFoundException Thrown if the realm is incorrect or the URI format is wrong, or if the command 
+	 * does not validate against the list of expected values.
+	 */
 	public AnsiURL(HttpServletRequest request, String expectedRealm, String[] expectedCommandList) throws ResourceNotFoundException {
 		super();
 		this.filterList = new ArrayList<String>();
