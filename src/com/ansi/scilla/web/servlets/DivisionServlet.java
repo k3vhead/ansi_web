a8f9ecffe9d78d3b64ec3cd3bed39b0f80769d9e
DC Lewis <dclewis@thewebthing.com>
2017-02-06 05:58:09 -0600

    Validate format on division code

--- src/com/ansi/scilla/web/servlets/DivisionServlet.java
+++ src/com/ansi/scilla/web/servlets/DivisionServlet.java
@@ -34,19 +34,17 @@ import com.thewebthing.commons.db2.RecordNotFoundException;
 
 
 /**
- * The url for delete will be of the form /division/<table>/<field>/<value>
+ * The url for HTTP DELETE will be of the form /division/&lt;id&gt;
  * 
- * The url for get will be one of:
- * 		/division    (retrieves everything)
- * 		/division/<table>      (filters division table by tablename)
- * 		/division/<table>/<field>	(filters division table tablename and field
- * 		/division/<table>/<field>/<value>	(retrieves a single record)
+ * The url for HTTP GET will be one of:
+ * 		/division/list    (retrieves everything)
+ * 		/division/&lt;id&gt;      (filters division table by id)
  * 
- * The url for adding a new record will be a POST to:
- * 		/division/new   with parameters in the JSON
+ * The url for adding a new record will be an HTTP POST to:
+ * 		/division/add   with parameters in the JSON
  * 
- * The url for update will be a POST to:
- * 		/division/<table>/<field>/<value> with parameters in the JSON
+ * The url for update will be an HTTP POST to:
+ * 		/division/&lt;id&gt; with parameters in the JSON
  * 
  * 
  * @author dclewis
@@ -137,111 +135,6 @@ public class DivisionServlet extends AbstractServlet {
 
 	}
 
-	/*
-	@Override
-	protected void doDelete(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		
-		String url = request.getRequestURI();
-		int idx = url.indexOf("/division/");
-		if ( idx > -1 ) {
-			// we're in the right place
-			Connection conn = null;
-			try {
-				conn = AppUtils.getDBCPConn();
-				conn.setAutoCommit(false);
-				
-				// Figure out what we've got:
-				String myString = url.substring(idx + "/division/".length());
-				
-				String[] urlPieces = myString.split("/");
-				String command = urlPieces[0];
-				
-				if ( StringUtils.isBlank(command) || ! StringUtils.isNumeric(command)) {
-					super.sendNotFound(response);
-				} else {
-					try {
-						doDeleteWork(conn, Integer.valueOf(command));
-						conn.commit();
-						DivisionResponse divisionResponse = new DivisionResponse();
-						super.sendResponse(conn, response, ResponseCode.SUCCESS, divisionResponse);
-					} catch (InvalidDeleteException e) {
-						String message = AppUtils.getMessageText(conn, MessageKey.DELETE_FAILED, "Invalid Delete");
-						WebMessages webMessages = new WebMessages();
-						webMessages.addMessage(WebMessages.GLOBAL_MESSAGE, message);
-						DivisionResponse divisionResponse = new DivisionResponse();
-						divisionResponse.setWebMessages(webMessages);
-						super.sendResponse(conn, response, ResponseCode.EDIT_FAILURE, divisionResponse);
-					} catch(RecordNotFoundException recordNotFoundEx) {
-						super.sendNotFound(response);
-					}
-				}
-			} catch ( Exception e) {
-				AppUtils.logException(e);
-				throw new ServletException(e);
-			} finally {
-				AppUtils.closeQuiet(conn);
-			}
-			
-		} else {
-			super.sendNotFound(response);
-		}
-		
-	}
-	
-	@Override
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		
-		String url = request.getRequestURI();
-		int idx = url.indexOf("/division/");
-		if ( idx > -1 ) {
-			String queryString = request.getQueryString();
-			
-			// Figure out what we've got:
-			// "myString" is the piece of the URL that we actually care about
-			String myString = url.substring(idx + "/division/".length());
-			String[] urlPieces = myString.split("/");
-			String command = urlPieces[0];
-
-			Connection conn = null;
-			try {
-				if ( StringUtils.isBlank(command)) {
-					throw new RecordNotFoundException();
-				}
-				conn = AppUtils.getDBCPConn();
-
-				DivisionListResponse divisionListResponse = doGetWork(conn, myString, queryString);
-				super.sendResponse(conn, response, ResponseCode.SUCCESS, divisionListResponse);
-			} catch(RecordNotFoundException recordNotFoundEx) {
-				super.sendNotFound(response);
-			} catch ( Exception e) {
-				AppUtils.logException(e);
-				throw new ServletException(e);
-			} finally {
-				AppUtils.closeQuiet(conn);
-			}
-
-		} else {
-			super.sendNotFound(response);
-		}
-	}
-
-	public DivisionListResponse doGetWork(Connection conn, String url, String qs) throws RecordNotFoundException, Exception {
-		DivisionListResponse divisionListResponse = new DivisionListResponse();
-		String[] x = url.split("/");
-		if(x[0].equals(ACTION_IS_LIST)){
-			divisionListResponse = new DivisionListResponse(conn);
-		} else if (StringUtils.isNumeric(x[0])) {
-			Integer divisionId = Integer.valueOf(x[0]);
-			divisionListResponse = new DivisionListResponse(conn, divisionId);
-		} else {
-			throw new RecordNotFoundException();
-		}
-		return divisionListResponse;
-		
-	}
-	*/
 	
 	public void doDeleteWork(Connection conn, Integer divisionId) throws RecordNotFoundException, InvalidDeleteException, Exception {
 
@@ -385,131 +278,6 @@ public class DivisionServlet extends AbstractServlet {
 		super.sendResponse(conn, response, responseCode, divisionResponse);
 	}
 
-	/*
-	@Override
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		
-		SessionUser sessionUser = AppUtils.getSessionUser(request);
-		String url = request.getRequestURI();
-		
-		Connection conn = null;
-		try {
-			conn = AppUtils.getDBCPConn();
-			conn.setAutoCommit(false);
-
-			// figure out if this is an "add" or an "update"
-			int idx = url.indexOf("/division/");
-			String myString = url.substring(idx + "/division/".length());		
-			String[] urlPieces = myString.split("/");
-			String command = urlPieces[0];
-			String jsonString = super.makeJsonString(request);
-			try {
-				DivisionRequest divisionRequest = (DivisionRequest) AppUtils.json2object(jsonString, DivisionRequest.class);
-				processPostRequest(conn, response, command, sessionUser, divisionRequest);
-			} catch ( InvalidFormatException formatException) {
-				processBadPostRequest(conn, response, formatException);
-			}
-		} catch ( Exception e ) {
-			AppUtils.logException(e);
-			AppUtils.rollbackQuiet(conn);
-			throw new ServletException(e);
-		} finally {
-			AppUtils.closeQuiet(conn);
-		}
-	}
-	*/
-		
-	private void processPostRequest(Connection conn, HttpServletResponse response, String command, SessionUser sessionUser, DivisionRequest divisionRequest) throws Exception {	
-			Division division = null;
-			ResponseCode responseCode = null;
-			if ( command.equals(ACTION_IS_ADD) ) {
-				WebMessages webMessages = validateAdd(conn, divisionRequest);
-				if (webMessages.isEmpty()) {
-					webMessages = validateFormat(conn, divisionRequest);
-				}
-				if (webMessages.isEmpty()) {
-					try {
-						division = doAdd(conn, divisionRequest, sessionUser);
-						String message = AppUtils.getMessageText(conn, MessageKey.SUCCESS, "Success!");
-						responseCode = ResponseCode.SUCCESS;
-						webMessages.addMessage(WebMessages.GLOBAL_MESSAGE, message);
-						conn.commit();
-					} catch ( DuplicateEntryException e ) {
-						String messageText = AppUtils.getMessageText(conn, MessageKey.DUPLICATE_ENTRY, "Record already Exists");
-						webMessages.addMessage(WebMessages.GLOBAL_MESSAGE, messageText);
-						responseCode = ResponseCode.EDIT_FAILURE;
-					} catch ( Exception e ) {
-						responseCode = ResponseCode.SYSTEM_FAILURE;
-						AppUtils.logException(e);
-						String messageText = AppUtils.getMessageText(conn, MessageKey.INSERT_FAILED, "Insert Failed");
-						webMessages.addMessage(WebMessages.GLOBAL_MESSAGE, messageText);
-					}
-				} else {
-					responseCode = ResponseCode.EDIT_FAILURE;
-				}
-				
-				
-				DivisionResponse divisionResponse = new DivisionResponse();
-				if ( division != null ) {
-					divisionResponse = new DivisionResponse(conn, division);
-				}
-				if ( ! webMessages.isEmpty()) {
-					divisionResponse.setWebMessages(webMessages);
-				}
-				super.sendResponse(conn, response, responseCode, divisionResponse);
-			
-			/*
-			This is the Update portion of the doPost
-			*/
-			} else if ( StringUtils.isNumeric(command) ) {   
-				WebMessages webMessages = validateAdd(conn, divisionRequest);
-				if (webMessages.isEmpty()) {
-					webMessages = validateFormat(conn, divisionRequest);
-				}
-				if (webMessages.isEmpty()) {
-					try {
-						Division key = new Division();
-						key.setDivisionId(Integer.valueOf(command));
-						division = doUpdate(conn, key, divisionRequest, sessionUser);
-						String message = AppUtils.getMessageText(conn, MessageKey.SUCCESS, "Success!");
-						responseCode = ResponseCode.SUCCESS;
-						webMessages.addMessage(WebMessages.GLOBAL_MESSAGE, message);
-						conn.commit();
-					} catch ( RecordNotFoundException e ) {
-						super.sendNotFound(response);
-						conn.rollback();
-					} catch ( Exception e) {
-						responseCode = ResponseCode.SYSTEM_FAILURE;
-						AppUtils.logException(e);
-						String messageText = AppUtils.getMessageText(conn, MessageKey.INSERT_FAILED, "Insert Failed");
-						webMessages.addMessage(WebMessages.GLOBAL_MESSAGE, messageText);
-						conn.rollback();
-					}
-				} else {
-					responseCode = ResponseCode.EDIT_FAILURE;
-				}
-				/* DivisionResponse divisionResponse = null;
-				if ( division != null ) {
-					divisionResponse = new DivisionResponse(conn, division);
-				}
-				if ( ! webMessages.isEmpty()) {
-					divisionResponse.setWebMessages(webMessages);
-				} */
-				
-				DivisionResponse divisionResponse = new DivisionResponse();
-				if ( division != null ) {
-					divisionResponse = new DivisionResponse(conn, division);
-				}
-				if ( ! webMessages.isEmpty()) {
-					divisionResponse.setWebMessages(webMessages);
-				}
-				
-				super.sendResponse(conn, response, responseCode, divisionResponse);
-			} else {
-				super.sendNotFound(response);
-			}
-	}
 
 	private void processBadPostRequest(Connection conn, HttpServletResponse response, InvalidFormatException formatException) throws Exception {
 		WebMessages webMessages = new WebMessages();
@@ -594,10 +362,10 @@ public class DivisionServlet extends AbstractServlet {
 	
 	protected WebMessages validateFormat(Connection conn, DivisionRequest divisionRequest) throws Exception {
 		WebMessages webMessages = new WebMessages();
-		List<String> missingFields = super.validateFormat(divisionRequest);
-		if ( ! missingFields.isEmpty() ) {
-			String messageText = AppUtils.getMessageText(conn, MessageKey.MISSING_DATA, "Required Entry");
-			for ( String field : missingFields ) {
+		List<String> badFormatFields = super.validateFormat(divisionRequest);
+		if ( ! badFormatFields.isEmpty() ) {
+			String messageText = AppUtils.getMessageText(conn, MessageKey.INVALID_DATA, "Invalid Format");
+			for ( String field : badFormatFields ) {
 				webMessages.addMessage(field, messageText);
 			}
 		}
