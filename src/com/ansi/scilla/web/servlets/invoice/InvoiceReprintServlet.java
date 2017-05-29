0a65d6faaf0f7e28c58290a7c281bf51fa33607f
DC Lewis <dclewis@thewebthing.com>
2017-05-29 09:20:07 -0500

    Reprint a single invoice Minor tweaks to global look & feel 	modified:   WebContent/WEB-INF/tags/webthing/buildDate.tag 	modified:   WebContent/WEB-INF/web.xml 	modified:   WebContent/css/style.css 	modified:   WebContent/invoiceLookup.jsp 	modified:   WebContent/invoicePrint.jsp 	modified:   WebContent/js/invoicePrint.js 	modified:   WebContent/layout.jsp 	modified:   src/com/ansi/scilla/web/response/invoice/InvoiceLookupResponseItem.java 	new file:   src/com/ansi/scilla/web/servlets/invoice/InvoiceReprintServlet.java

---
+++ src/com/ansi/scilla/web/servlets/invoice/InvoiceReprintServlet.java
@@ -0,0 +1,134 @@
+package com.ansi.scilla.web.servlets.invoice;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ansi.scilla.common.db.Invoice;
+import com.ansi.scilla.common.db.PermissionLevel;
+import com.ansi.scilla.common.invoice.InvoiceUtils;
+import com.ansi.scilla.web.common.AnsiURL;
+import com.ansi.scilla.web.common.AppUtils;
+import com.ansi.scilla.web.common.Permission;
+import com.ansi.scilla.web.common.ResponseCode;
+import com.ansi.scilla.web.common.WebMessages;
+import com.ansi.scilla.web.exceptions.ExpiredLoginException;
+import com.ansi.scilla.web.exceptions.NotAllowedException;
+import com.ansi.scilla.web.exceptions.TimeoutException;
+import com.ansi.scilla.web.response.invoice.InvoicePrintResponse;
+import com.ansi.scilla.web.servlets.AbstractServlet;
+import com.ansi.scilla.web.struts.SessionData;
+import com.ansi.scilla.web.struts.SessionUser;
+import com.thewebthing.commons.db2.RecordNotFoundException;
+
+
+
+public class InvoiceReprintServlet extends AbstractServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		AnsiURL ansiURL = null; 
+		Connection conn = null;
+		try {
+			try{
+				conn = AppUtils.getDBCPConn();
+				conn.setAutoCommit(false);
+				ansiURL = new AnsiURL(request, "invoiceReprint", (String[])null); //  .../ticket/etc
+				SessionData sessionData = AppUtils.validateSession(request, Permission.INVOICE, PermissionLevel.PERMISSION_LEVEL_IS_WRITE);
+				Invoice invoice = validateInvoice(conn, ansiURL.getId());
+				SessionUser sessionUser = sessionData.getUser();				
+				processUpdate(conn, request, response, invoice, sessionUser);
+				conn.commit();
+			} catch ( RecordNotFoundException e ) {
+				super.sendNotFound(response);
+			} catch (TimeoutException | NotAllowedException | ExpiredLoginException e1) {
+				super.sendForbidden(response);
+			}
+		} catch ( Exception e) {
+			AppUtils.logException(e);
+			AppUtils.rollbackQuiet(conn);
+			throw new ServletException(e);
+		} finally {
+			AppUtils.closeQuiet(conn);
+		}
+	}
+
+
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {		
+		super.sendNotAllowed(response);
+	}
+
+
+	@Override
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		super.sendNotAllowed(response);
+	}
+
+
+	private Invoice validateInvoice(Connection conn, Integer id) throws RecordNotFoundException, Exception {
+		Invoice invoice = new Invoice();
+		invoice.setInvoiceId(id);
+		invoice.selectOne(conn);
+		return invoice;
+	}
+
+
+	private void processUpdate(Connection conn, HttpServletRequest request, HttpServletResponse response, Invoice invoice, SessionUser sessionUser) throws Exception {
+		WebMessages webMessages = new WebMessages();
+		Date today = Calendar.getInstance(TimeZone.getTimeZone("America/Chicago")).getTime();
+		SimpleDateFormat fileDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		InvoicePrintResponse data = new InvoicePrintResponse();
+		String pathTranslated = request.getPathTranslated();
+		Integer invoiceId = invoice.getInvoiceId();
+		String invoicePathName = pathTranslated.substring(0, pathTranslated.length() - String.valueOf(invoiceId).length());
+		invoicePathName = invoicePathName + "invoicePDF";
+		File invoicePath = new File(invoicePathName);
+		if ( ! invoicePath.exists()) {
+			invoicePath.mkdirs();
+		}
+		String fileDate = fileDateFormat.format(today);
+		String invoiceFileName = invoice.getInvoiceId() + "_" + fileDate + ".pdf";
+
+
+		ByteArrayOutputStream baos = InvoiceUtils.reprintInvoice(conn, invoice);
+		
+		
+		FileOutputStream os = new FileOutputStream(new File(invoicePathName + "/" + invoiceFileName));
+		baos.writeTo(os);
+		os.flush();
+		os.close();
+		
+		
+		data.setInvoiceFile("invoicePDF/" + invoiceFileName);
+		data.setWebMessages(webMessages);
+		super.sendResponse(conn, response, ResponseCode.SUCCESS, data);
+
+		
+	}
+
+
+	
+
+
+	
+	
+}
