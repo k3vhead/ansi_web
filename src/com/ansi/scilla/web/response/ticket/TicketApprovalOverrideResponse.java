c63e9b3133869816b53ec7bf5205351547611833
DC Lewis <dclewis@thewebthing.com>
2017-05-29 18:22:51 -0500

    Allow ticket return approvals to be changed for tickets with status 'C'ompleted 	modified:   WebContent/WEB-INF/web.xml 	modified:   WebContent/ticketReturn.jsp 	new file:   src/com/ansi/scilla/web/request/ticket/TicketApprovalOverride.java 	new file:   src/com/ansi/scilla/web/response/ticket/TicketApprovalOverrideResponse.java 	new file:   src/com/ansi/scilla/web/servlets/tickets/TicketApprovalOverrideServlet.java

---
+++ src/com/ansi/scilla/web/response/ticket/TicketApprovalOverrideResponse.java
@@ -0,0 +1,16 @@
+package com.ansi.scilla.web.response.ticket;
+
+import com.ansi.scilla.web.response.MessageResponse;
+
+public class TicketApprovalOverrideResponse extends MessageResponse {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public TicketApprovalOverrideResponse(){
+		super();
+	}
+	
+}
