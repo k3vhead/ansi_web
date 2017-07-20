e5e39ad0f41f3d3a1b06e7f0fe9875e7e2d6dd0f
DC Lewis <dclewis@thewebthing.com>
2017-07-20 07:36:34 -0500

    Code cleanup -- add stubs to get rid of compile errors in sample code 	new file:   test/com/ansi/scilla/web/test/sample/AppUtils.java 	new file:   test/com/ansi/scilla/web/test/sample/MembershipYear.java 	new file:   test/com/ansi/scilla/web/test/sample/ReportConfig.java 	renamed:    test/com/ansi/scilla/web/test/RosterServlet.java -> test/com/ansi/scilla/web/test/sample/RosterServlet.java 	new file:   test/com/ansi/scilla/web/test/sample/RosterUtils.java 	new file:   test/com/ansi/scilla/web/test/sample/SessionData.java

---
+++ test/com/ansi/scilla/web/test/sample/AppUtils.java
@@ -0,0 +1,16 @@
+package com.ansi.scilla.web.test.sample;
+
+import java.sql.Connection;
+
+public class AppUtils {
+
+	public static Connection getConn() {
+		return null;
+	}
+
+	public static void logException(Exception e) {
+		// TODO Auto-generated method stub
+		
+	}		
+}
+
---
+++ test/com/ansi/scilla/web/test/sample/SessionData.java
@@ -0,0 +1,21 @@
+package com.ansi.scilla.web.test.sample;
+
+import java.util.List;
+
+import com.ansi.scilla.web.test.sample.RosterServlet.Member;
+
+public class SessionData {
+
+	public static final String KEY = null;
+
+	public Member getLogin() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<String> getPermissions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
