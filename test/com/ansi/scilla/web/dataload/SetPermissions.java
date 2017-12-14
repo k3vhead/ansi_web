87a4b9007245a310cdce729fc207e6f82a75687a
DC Lewis <dclewis@thewebthing.com>
2017-12-14 07:14:38 -0600

    Create permission group and all the associated folderol for invoice date edit in the ticket override 	modified:   test/com/ansi/scilla/web/dataload/SetPermissions.java

--- test/com/ansi/scilla/web/dataload/SetPermissions.java
+++ test/com/ansi/scilla/web/dataload/SetPermissions.java
@@ -2,7 +2,9 @@ package com.ansi.scilla.web.dataload;
 
 import java.sql.Connection;
 
+import com.ansi.scilla.common.db.PermissionGroup;
 import com.ansi.scilla.common.db.PermissionGroupLevel;
+import com.ansi.scilla.common.db.PermissionLevel;
 import com.ansi.scilla.web.common.utils.AppUtils;
 import com.ansi.scilla.web.common.utils.Permission;
 
@@ -21,14 +23,30 @@ public class SetPermissions {
 	public void go() throws Exception {
 		Connection conn = null;
 		try {
-			conn = AppUtils.getDevConn();
+			conn = AppUtils.getProdConn();
 			conn.setAutoCommit(false);
+			
+			PermissionGroup pg = new PermissionGroup();
+			pg.setAddedBy(5);
+			pg.setDescription("Lee, Danielle, Gary");
+			pg.setName("Special Override");
+			pg.setStatus(PermissionGroup.STATUS_IS_ACTIVE);
+			pg.setUpdatedBy(5);
+			Integer pgId = pg.insertWithKey(conn);
+			System.out.println("ID: " + pgId);
+			
+			PermissionLevel pl = new PermissionLevel();
+			pl.setAddedBy(5);
+			pl.setLevel(PermissionLevel.PERMISSION_LEVEL_IS_WRITE);
+			pl.setPermissionName(Permission.TICKET_SPECIAL_OVERRIDE.toString());
+			pl.setUpdatedBy(5);
+			pl.insertWithNoKey(conn);
 			
 			for ( Permission p : Permission.values() ) {
 				System.out.println(p);
 				PermissionGroupLevel pgl = new PermissionGroupLevel();
 				pgl.setAddedBy(5);
-				pgl.setPermissionGroupId(4); 	//special override group
+				pgl.setPermissionGroupId(pgId); 	//special override group
 				pgl.setPermissionLevel(1);
 				pgl.setPermissionName(p.toString());
 				pgl.setUpdatedBy(5);
@@ -36,7 +54,9 @@ public class SetPermissions {
 			}
 			conn.commit();
 		} catch ( Exception e) {
-			conn.rollback();
+			if ( conn != null ) {
+				conn.rollback();
+			}
 			throw(e);
 		}
 	}
