8be56dedf0f077a49f41025bfca3fc2b4d798144
DC Lewis <dclewis@thewebthing.com>
2018-10-28 15:29:44 -0500

    Simplify the permission response 	new file:   src/com/ansi/scilla/web/permission/response/PermissionDisplayItem.java 	modified:   src/com/ansi/scilla/web/permission/response/PermissionListResponse.java

---
+++ src/com/ansi/scilla/web/permission/response/PermissionDisplayItem.java
@@ -0,0 +1,51 @@
+package com.ansi.scilla.web.permission.response;
+
+import com.ansi.scilla.common.ApplicationObject;
+import com.ansi.scilla.web.common.utils.Permission;
+
+public class PermissionDisplayItem extends ApplicationObject {
+
+	private static final long serialVersionUID = 1L;
+
+	private String permissionName;
+	private String description;
+	private Boolean included;
+	
+	
+	public PermissionDisplayItem() {
+		super();
+	}
+	
+	public PermissionDisplayItem(Permission permission, Boolean included) {
+		this();
+		this.permissionName = permission.name();
+		this.description = permission.getDescription();
+		this.included = included;
+	}
+
+	public String getPermissionName() {
+		return permissionName;
+	}
+
+	public void setPermissionName(String permissionName) {
+		this.permissionName = permissionName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Boolean getIncluded() {
+		return included;
+	}
+
+	public void setIncluded(Boolean included) {
+		this.included = included;
+	}
+	
+	
+}
--- src/com/ansi/scilla/web/permission/response/PermissionListResponse.java
+++ src/com/ansi/scilla/web/permission/response/PermissionListResponse.java
@@ -2,146 +2,102 @@ package com.ansi.scilla.web.permission.response;
 
 import java.sql.Connection;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Transformer;
 
-import com.ansi.scilla.web.code.response.CodeResponseItem;
-import com.ansi.scilla.web.common.response.MessageResponse;
-import com.ansi.scilla.web.common.utils.Permission;
-import com.ansi.scilla.web.permission.query.PermissionItemLookupSearch;
-import com.ansi.scilla.web.permission.query.PermissionItemSearchResponse;
-import com.ansi.scilla.common.db.Code;
 import com.ansi.scilla.common.db.PermissionGroupLevel;
+import com.ansi.scilla.web.common.response.MessageResponse;
+import com.ansi.scilla.web.common.utils.AppUtils;
+import com.ansi.scilla.web.common.utils.Permission;
 
-//import com.ansi.scilla.web.test.TestServlet;
 
 public class PermissionListResponse extends MessageResponse {
-	/**
-	 * 
-	 * @author kwagner
-	 * 
-	 * 		A class extending MessageResponse that contains a 
-	 * 		list of Permissions for a given group id.. 
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	//	protected final Logger logger = LogManager.getLogger(TestServlet.class);
+	private List<List<PermissionDisplayItem>> permissionList;
 	
-	private List<PermissionGroupItem> permissionGroupLevelList;
-	
-	
-	// Permission is an ENUM class.. 
-	private List<Permission> permissionList;
-
 	public PermissionListResponse() {
 		super();
-		this.permissionList = new ArrayList<Permission>();
-		for (Permission p : Permission.values()) {
-			permissionList.add(p);
-		}
+		makeMasterList(new ArrayList<Permission>());
+	}
+
+	public PermissionListResponse(Connection conn, Integer permissionGroupId ) throws Exception {
+		super();
+		List<Permission> permissionList = makeGroupList(conn, permissionGroupId);
+		makeMasterList(permissionList);
 	}
 	
-	public PermissionListResponse(Connection conn) throws Exception{
-		Integer offset=0; 
-		Integer rowCount=1000;
-
-		this.permissionList = new ArrayList<Permission>();
-		this.permissionGroupLevelList = new ArrayList<PermissionGroupItem>();
-		
-		PermissionItemLookupSearch lookup = new PermissionItemLookupSearch();
-		List<PermissionItemSearchResponse> permissionItemSearchResponses = lookup.select(conn, offset, rowCount); 
-		
-		PermissionGroupItem permissionGroupItem;
-		
-		for(PermissionItemSearchResponse response:permissionItemSearchResponses) {
-			// Add it's List of Permissions to the complete list of permissions.
-			permissionGroupItem = new PermissionGroupItem();
-			permissionGroupItem.setPermissionGroupId(response.getPermissionGroupId());
-			permissionGroupItem.setPermissionLevel(response.getPermissionLevel());
-			permissionGroupItem.setPermissionName(response.getPermissionName());
-			
-			// Add it's List of Permissions to the complete list of permissions.
-			this.permissionGroupLevelList.add(permissionGroupItem);
-			this.permissionList.addAll(response.getPermissionList());
-		}	
-		
-		/*
-		this.codeList = new ArrayList<CodeResponseItem>();
-		for ( Code code : codeList ) {
-			this.codeList.add(new CodeResponseItem(code));
-		}
-		Collections.sort(this.codeList);
-	    */		
-	}
-
-	public PermissionListResponse(Connection conn, int permGroupId) throws Exception {
-		Integer offset=0; 
-		Integer rowCount=1000;
-		
-		//17this.permissionItemList = new ArrayList<PermissionItem>();
-		this.permissionList = new ArrayList<Permission>();
-		this.permissionGroupLevelList = new ArrayList<PermissionGroupItem>();
-		
-		// Instantiate PermissionItemLookupSearch, an Extended ReportQuery Object. 
-		   // Pass it the groupID so that it can use it to build the SQL statements 
-		PermissionItemLookupSearch lookup = new PermissionItemLookupSearch(permGroupId);
-
-		// Execute the query and get the info from the database.. 
-		   // PermissionItemSearchResponse is also an extended ReportQuery Class
-		   // but it is only used to bind to records in the database pass back by the select() method.
-				
-		List<PermissionItemSearchResponse> permissionItemSearchResponses = lookup.select(conn, offset, rowCount); 
-		
-		PermissionGroupItem permissionGroupLevelItem;
-		// For each record(PermissionItemSearchResponse) returned by the select.. 
-		for(PermissionItemSearchResponse response:permissionItemSearchResponses) {
-			permissionGroupLevelItem = new PermissionGroupItem();
-			permissionGroupLevelItem.setPermissionGroupId(response.getPermissionGroupId());
-			permissionGroupLevelItem.setPermissionLevel(response.getPermissionLevel());
-			permissionGroupLevelItem.setPermissionName(response.getPermissionName());
-			
-			// Add it's List of Permissions to the complete list of permissions.
-			this.permissionGroupLevelList.add(permissionGroupLevelItem);
-			this.permissionList.addAll(response.getPermissionList());
-		}	
-	}
-	
-	
-	/*
-	public PermissionListResponse(Connection conn) throws Exception {
-		Integer offset=0; 
-		Integer rowCount=1000;
-		
-		//17this.permissionItemList = new ArrayList<PermissionItem>();
-		this.permissionList = new ArrayList<Permission>();
-		
-		// Instantiate PermissionItemLookupSearch, an Extended ReportQuery Object. 
-		   // Pass it the groupID so that it can use it to build the SQL statements 
-		PermissionItemLookupSearch lookup = new PermissionItemLookupSearch();
-		
-	    // Execute the query and get the info from the database.. 
-		   // PermissionItemSearchResponse is also an extended ReportQuery Class
-		   // but it is only used to bind to records in the database pass back by the select() method.
-		List<PermissionItemSearchResponse> permissionItemSearchResponses = lookup.select(conn, offset, rowCount); 
-		
-		// For each record(PermissionItemSearchResponse) returned by the select.. 
-		for(PermissionItemSearchResponse response:permissionItemSearchResponses) {
-
-			// Add it's List of Permissions to the complete list of permissions. 
-			this.permissionList.addAll(response.getPermissionList());
-		}	
-	}
-	*/
-
-	public List<Permission> getPermissionList() {
+	public List<List<PermissionDisplayItem>> getPermissionList() {
 		return permissionList;
 	}
-	
-	public List<PermissionGroupItem>  getPermissionGroupLevelList(){
-		return this.permissionGroupLevelList;
+
+	public void setPermissionList(List<List<PermissionDisplayItem>> permissionList) {
+		this.permissionList = permissionList;
 	}
+
+	
+
+	
+	private void makeMasterList(List<Permission> groupPermissionList) {
+		permissionList = new ArrayList<List<PermissionDisplayItem>>();
+
+		for ( Permission p : Permission.values() ) {
+			if ( p.getParent() == null ) {
+				List<PermissionDisplayItem> sublist = new ArrayList<PermissionDisplayItem>();
+				sublist.add(new PermissionDisplayItem(p, false));
+				makeSubList(p, sublist, groupPermissionList);
+				permissionList.add(sublist);
+			}
+		}
+	}
+
+	private  void makeSubList(Permission p, List<PermissionDisplayItem> sublist, List<Permission> groupPermissionList) {
+		for ( Permission permission : p.makeChildList() ) {
+			sublist.add(new PermissionDisplayItem(permission, groupPermissionList.contains(permission)));
+			makeSubList(permission, sublist, groupPermissionList);
+		}
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	private List<Permission> makeGroupList(Connection conn, Integer permissionGroupId) throws Exception{
+		PermissionGroupLevel key = new PermissionGroupLevel();
+		key.setPermissionGroupId(permissionGroupId);
+		List<PermissionGroupLevel> groupPermissionList = PermissionGroupLevel.cast(key.selectSome(conn));
+		List<Permission> permissionList = (List<Permission>) CollectionUtils.collect(groupPermissionList.iterator(), new Group2Permission());
+		return permissionList;
+	}
+
+	
+	public class Group2Permission implements Transformer {
+
+		@Override
+		public Object transform(Object arg0) {
+			PermissionGroupLevel group = (PermissionGroupLevel)arg0;
+			Permission p = Permission.valueOf(group.getPermissionName());
+			return p;
+		}
+	}
+	
+	
+//	public static void main(String[] args) {
+//		Connection conn = null;		
+//		try {
+//			conn = AppUtils.getDevConn();
+//			PermissionListResponse x = new PermissionListResponse(conn, 7);
+//			for ( List<PermissionDisplayItem> itemList : x.getPermissionList() ) {
+//				for ( PermissionDisplayItem item : itemList ) {
+//					System.out.println(item.getPermissionName() + "\t" + item.getIncluded());
+//				}
+//				System.out.println("======================");
+//			}
+//		} catch ( Exception e) {
+//			e.printStackTrace();
+//		} finally {
+//			AppUtils.closeQuiet(conn);
+//		}
+//		
+//		
+//	}
 }
