0ea8f3fbf4d7742438966296ac39f990654d318e
Joshua Lewis <jwlewis@tardis2.thewebthing.com>
2018-12-18 11:48:32 -0600

    DivisionUserServlet: doGet, doGetWork

--- src/com/ansi/scilla/web/user/servlet/DivisionUserServlet.java
+++ src/com/ansi/scilla/web/user/servlet/DivisionUserServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Level;
 
+import com.ansi.scilla.common.db.DivisionUser;
 import com.ansi.scilla.common.db.User;
 import com.ansi.scilla.web.common.response.ResponseCode;
 import com.ansi.scilla.web.common.response.WebMessages;
@@ -24,8 +25,8 @@ import com.ansi.scilla.web.exceptions.NotAllowedException;
 import com.ansi.scilla.web.exceptions.ResourceNotFoundException;
 import com.ansi.scilla.web.exceptions.TimeoutException;
 import com.ansi.scilla.web.user.request.AnsiUserRequest;
+import com.ansi.scilla.web.user.request.DivisionUserRequest;
 import com.ansi.scilla.web.user.response.DivisionUserResponse;
-import com.ansi.scilla.web.user.response.UserResponse;
 import com.thewebthing.commons.db2.RecordNotFoundException;
 
 public class DivisionUserServlet extends AbstractServlet {
@@ -52,43 +53,53 @@ public class DivisionUserServlet extends AbstractServlet {
 	
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		AnsiURL url = null;
-		Connection conn = null;
+//		AnsiURL url = null;
+//		Connection conn = null;
+//		
+//		try {
+//			conn = AppUtils.getDBCPConn();
+//			conn.setAutoCommit(false);
+//			SessionData sessionData = AppUtils.validateSession(request, Permission.USER_ADMIN_WRITE);
+//			SessionUser sessionUser = sessionData.getUser();
+//			String jsonString = super.makeJsonString(request);
+//			url = new AnsiURL(request, REALM, new String[] {ACTION_IS_ADD});
+//			if ( url.getId() == null && StringUtils.isBlank(url.getCommand())) {
+//				super.sendNotFound(response);
+//			} else {
+//				AnsiUserRequest userRequest = new AnsiUserRequest();
+//				AppUtils.json2object(jsonString, userRequest);
+//				User user = new User();
+//				if ( url.getId() == null ) {
+//					//addUser(conn, request, response, user, userRequest, sessionUser);
+//				} else {
+//					user.setUserId(url.getId());
+//					user.selectOne(conn);
+//					//updateUser(conn, request, response, user, userRequest, sessionUser);
+//				}
+//			}
+//		} catch (TimeoutException | NotAllowedException | ExpiredLoginException e) {
+//			super.sendForbidden(response);
+//		} catch (ResourceNotFoundException e) {
+//			super.sendNotAllowed(response);
+//		} catch ( RecordNotFoundException e ) {
+//			super.sendNotFound(response);
+//		} catch ( Exception e ) {
+//			AppUtils.logException(e);
+//			AppUtils.rollbackQuiet(conn);
+//			throw new ServletException(e);
+//		} finally {
+//			AppUtils.closeQuiet(conn);
+//		}
 		
 		try {
-			conn = AppUtils.getDBCPConn();
-			conn.setAutoCommit(false);
-			SessionData sessionData = AppUtils.validateSession(request, Permission.USER_ADMIN_WRITE);
-			SessionUser sessionUser = sessionData.getUser();
-			String jsonString = super.makeJsonString(request);
-			url = new AnsiURL(request, REALM, new String[] {ACTION_IS_ADD});
-			if ( url.getId() == null && StringUtils.isBlank(url.getCommand())) {
-				super.sendNotFound(response);
-			} else {
-				AnsiUserRequest userRequest = new AnsiUserRequest();
-				AppUtils.json2object(jsonString, userRequest);
-				User user = new User();
-				if ( url.getId() == null ) {
-					//addUser(conn, request, response, user, userRequest, sessionUser);
-				} else {
-					user.setUserId(url.getId());
-					user.selectOne(conn);
-					//updateUser(conn, request, response, user, userRequest, sessionUser);
-				}
-			}
+			AppUtils.validateSession(request, Permission.USER_ADMIN_WRITE);
+			doPostWork(request, response);
 		} catch (TimeoutException | NotAllowedException | ExpiredLoginException e) {
 			super.sendForbidden(response);
-		} catch (ResourceNotFoundException e) {
-			super.sendNotAllowed(response);
-		} catch ( RecordNotFoundException e ) {
-			super.sendNotFound(response);
-		} catch ( Exception e ) {
+		} catch ( Exception e) {
 			AppUtils.logException(e);
-			AppUtils.rollbackQuiet(conn);
 			throw new ServletException(e);
-		} finally {
-			AppUtils.closeQuiet(conn);
-		}
+		} 
 	}
 	
 	private void doGetWork(HttpServletRequest request, HttpServletResponse response) throws ServletException {
@@ -133,6 +144,63 @@ public class DivisionUserServlet extends AbstractServlet {
 		}
 	
 	}
+	
+	private void doPostWork(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+		Connection conn = null;
+		DivisionUserResponse userResponse = new DivisionUserResponse();
+		DivisionUserRequest userRequest = (DivisionUserRequest) request;
+		WebMessages messages = new WebMessages();
+		try {
+			conn = AppUtils.getDBCPConn();
+			String[] str = new String[0];
+			AnsiURL url = new AnsiURL(request, REALM, str);	
+			
+	
+			if( url.getId() == null && StringUtils.isBlank(url.getCommand())) {
+				logger.log(Level.DEBUG, "divisionUser servlet 43");
+				throw new ResourceNotFoundException();
+			} else if (url.getId() != null) {
+				logger.log(Level.DEBUG, "divisionUser servlet 46");
+				User ansiUser = new User();
+				ansiUser.setUserId(url.getId());
+				ansiUser.selectOne(conn);
+				if(userRequest.isActive()) {
+					doUpdate(url.getId(), userRequest.getDivisionId());
+				} else if(!userRequest.isActive()) {
+					//doUpdate(url.getId(), userRequest.getDivisionId());
+					doUpdate(null, null);
+				}
+				userResponse = new DivisionUserResponse(conn, url.getId());
+			} else {
+				// according to the URI parsing, this shouldn't happen, but it gives me warm fuzzies
+				throw new ResourceNotFoundException();
+			}
+			
+			logger.log(Level.DEBUG, "user servlet 55");
+			messages.addMessage(WebMessages.GLOBAL_MESSAGE, "Success");
+			userResponse.setWebMessages(messages);
+			super.sendResponse(conn, response, ResponseCode.SUCCESS, userResponse);
+		} catch(RecordNotFoundException e) {
+			logger.log(Level.DEBUG, "user servlet 60");
+			super.sendNotFound(response);
+		} catch(ResourceNotFoundException e) {
+			logger.log(Level.DEBUG, "user servlet 63");
+			super.sendNotFound(response);
+		} catch ( Exception e) {
+			AppUtils.logException(e);
+			throw new ServletException(e);
+		} finally {
+			AppUtils.closeQuiet(conn);
+		}
+	
+	}
+
+	private void doUpdate(Integer userId, Integer divisionId) {
+		DivisionUser divUser = new DivisionUser();
+		divUser.setUserId(userId);
+		divUser.setDivisionId(divisionId);
+	}
+	
 }
 
 
