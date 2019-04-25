f87341992d58efa92c972f0889fcf07a3a57c3d4
Joshua Lewis <jwlewis@tardis2.thewebthing.com>
2019-04-25 18:51:48 -0500

    UserLookupServlet: doPost, processUpdate, not done

--- src/com/ansi/scilla/web/user/servlet/UserLookupServlet.java
+++ src/com/ansi/scilla/web/user/servlet/UserLookupServlet.java
@@ -15,16 +15,29 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.ansi.scilla.common.db.Contact;
 import com.ansi.scilla.common.db.PermissionLevel;
+import com.ansi.scilla.common.db.User;
+import com.ansi.scilla.web.common.response.ResponseCode;
+import com.ansi.scilla.web.common.response.WebMessages;
 import com.ansi.scilla.web.common.servlet.AbstractServlet;
+import com.ansi.scilla.web.common.struts.SessionData;
+import com.ansi.scilla.web.common.struts.SessionUser;
+import com.ansi.scilla.web.common.utils.AnsiURL;
 import com.ansi.scilla.web.common.utils.AppUtils;
 import com.ansi.scilla.web.common.utils.Permission;
+import com.ansi.scilla.web.contact.request.ContactRequest;
+import com.ansi.scilla.web.contact.response.ContactResponse;
 import com.ansi.scilla.web.exceptions.ExpiredLoginException;
 import com.ansi.scilla.web.exceptions.NotAllowedException;
 import com.ansi.scilla.web.exceptions.TimeoutException;
 import com.ansi.scilla.web.user.query.UserLookup;
 import com.ansi.scilla.web.user.query.UserLookupItem;
+import com.ansi.scilla.web.user.request.AnsiUserRequest;
 import com.ansi.scilla.web.user.response.UserLookupJsonResponse;
+import com.ansi.scilla.web.user.response.UserResponse;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.thewebthing.commons.db2.RecordNotFoundException;
 
 public class UserLookupServlet extends AbstractServlet {
 
@@ -39,10 +52,92 @@ public class UserLookupServlet extends AbstractServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		super.sendNotAllowed(response);
+		super.sendNotAllowed(response); //get rid of
+		
+		AnsiURL url = null;
+		Connection conn = null;
+		
+		try {
+			conn = AppUtils.getDBCPConn();
+			conn.setAutoCommit(false);
+			
+			SessionData sessionData = AppUtils.validateSession(request, Permission.USER_ADMIN_WRITE);
+			SessionUser sessionUser = sessionData.getUser();
+
+			try {
+				String jsonString = super.makeJsonString(request);
+				AnsiUserRequest ansiUserRequest = (AnsiUserRequest)AppUtils.json2object(jsonString, ContactRequest.class);
+				url = new AnsiURL(request,"user", new String[] {ACTION_IS_ADD});
+
+				if ( url.getId() != null ) {
+					// THis is an update
+					processUpdate(conn, response, url.getId(), ansiUserRequest, sessionUser);
+				} else if ( url.getCommand().equalsIgnoreCase(ACTION_IS_ADD)) {
+					// this is an add
+					//processAdd(conn, response, ansiUserRequest, sessionUser);
+				} else {
+					// this is messed up
+					super.sendNotFound(response);
+				}
+			} catch ( InvalidFormatException e ) {
+				String badField = super.findBadField(e.toString());
+				ContactResponse data = new ContactResponse();
+				WebMessages messages = new WebMessages();
+				messages.addMessage(badField, "Invalid Format");
+				data.setWebMessages(messages);
+				super.sendResponse(conn, response, ResponseCode.EDIT_FAILURE, data);
+			} 
+//			catch (TimeoutException | NotAllowedException | ExpiredLoginException e) {
+//				super.sendForbidden(response);
+//			} catch ( RecordNotFoundException e ) {
+//				super.sendNotFound(response);
+//			}
+		} catch ( Exception e ) {
+			AppUtils.logException(e);
+			AppUtils.rollbackQuiet(conn);
+			throw new ServletException(e);
+		} finally {
+			AppUtils.closeQuiet(conn);
+		}
 	}
 	
 	
+	@SuppressWarnings("unchecked")
+	private void processUpdate(Connection conn, HttpServletResponse response, Integer id,
+			AnsiUserRequest ansiUserRequest, SessionUser sessionUser) throws Exception {
+		
+		User user = new User();
+		UserResponse data = new UserResponse();
+		user.setUserId(id);
+		user.selectOne(conn);  // this throws RecordNotFound, which is propagated up the line into a 404 return 
+		WebMessages webMessages = validateUpdate(conn, user, ansiUserRequest);
+		
+		if (webMessages.isEmpty()) {
+			user = doUpdate(conn, id, user, ansiUserRequest, sessionUser);
+			conn.commit();
+			webMessages.addMessage(WebMessages.GLOBAL_MESSAGE, "Update successful!");
+			data.setUserList((List<UserLookupItem>) user);
+			data.setWebMessages(webMessages);
+			super.sendResponse(conn, response, ResponseCode.SUCCESS, data);
+		} else {
+			data.setWebMessages(webMessages);
+			super.sendResponse(conn, response, ResponseCode.EDIT_FAILURE, data);
+		}
+	}
+
+	
+
+	private WebMessages validateUpdate(Connection conn, User user, AnsiUserRequest ansiUserRequest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private User doUpdate(Connection conn, Integer id, User user, AnsiUserRequest ansiUserRequest,
+			SessionUser sessionUser) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
