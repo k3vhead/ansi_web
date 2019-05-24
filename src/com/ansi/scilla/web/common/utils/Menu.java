699fbbb3621e26c0dc6faeeaaf4680f3c51403ed
DC Lewis <dclewis@thewebthing.com>
2019-05-24 17:10:50 -0500

    Menu enum 	new file:   src/com/ansi/scilla/web/common/utils/Menu.java

---
+++ src/com/ansi/scilla/web/common/utils/Menu.java
@@ -0,0 +1,58 @@
+package com.ansi.scilla.web.common.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum Menu {
+	DASHBOARD("Dashboard", null, null,"#"),
+	
+	
+	LOOKUPS("Lookups", null, null,"#"),
+	
+	
+	REPORTS("Reports", null, null,"#"),
+	
+	
+	QUICK_LINKS("Quick Links", null, null,"#"),
+	
+	
+	SETTINGS("Settings", null, Permission.SYSADMIN_READ,"#"),
+	TAX_MAINTENANCE("Tax Maintenance",SETTINGS,Permission.SYSADMIN_READ,"taxRateMaintenance.html"),
+	CODES("Codes",SETTINGS,Permission.SYSADMIN_READ,"taxRatcodeMaintenanceeMaintenance.html"),
+	DIVISIONS("Divisions",SETTINGS,Permission.SYSADMIN_READ,"divisionAdmin.html"),
+	USER_LOOKUP("User Lookup",SETTINGS,Permission.SYSADMIN_READ,"userLookup.html"),
+	PERMISSIONS("Permissions",SETTINGS,Permission.SYSADMIN_READ,"permissionGroup.html"),
+
+	
+	MY_ANSI("My ANSI", null, null,"#"),
+	
+	
+	;
+	
+	private final String displayText;
+	private final Menu parent;
+	private final Permission permissionRequired;
+	private final String link;
+	
+	private Menu(String displayText, Menu parent, Permission permissionRequired, String link) {
+		this.displayText = displayText;
+		this.parent = parent;
+		this.permissionRequired = permissionRequired;
+		this.link = link; 
+	}
+	
+	public String getDisplayText() { return this.displayText; }
+	public Menu getParent() { return this.parent; }
+	public Permission getPermissionRequired() { return this.permissionRequired; }
+	public String getLink() { return this.link; }
+	
+	public static List<Menu> topLevel() {
+		List<Menu> topLevel = new ArrayList<Menu>();
+		for ( Menu menu : Menu.values()) {
+			if ( menu.getParent() == null ) {
+				topLevel.add(menu);
+			}
+		}
+		return topLevel;
+	}
+}
