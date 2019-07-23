39c553dd7bf7b94ab96b0c3a59e012d07b59a7a9
Joshua Lewis <jwlewis@tardis2.thewebthing.com>
2019-07-23 11:49:17 -0500

    LocaleDivisionResponse

--- src/com/ansi/scilla/web/locale/response/LocaleDivisionResponse.java
+++ src/com/ansi/scilla/web/locale/response/LocaleDivisionResponse.java
@@ -11,11 +11,23 @@ public class LocaleDivisionResponse extends MessageResponse {
 
 	private static final long serialVersionUID = 1L;
 	
-	public static final String DIVISION_ID = "division_id";
-	public static final String LOCALE_ID = "locale_id";
-	public static final String EFFECTIVE_START_DATE = "effective_start_date";
-	public static final String EFFECTIVE_STOP_DATE = "effective_stop_date";
-	public static final String ADDRESS_ID = "address_id";
+	public static final String DIVISION_ID = "locale_division.division_id";
+	public static final String LOCALE_ID = "locale_division.locale_id";
+	public static final String EFF_START_DATE = "locale_division.effective_start_date";
+	public static final String EFF_STOP_DATE = "locale_division.effective_stop_date"; 
+	public static final String ADDRESS_ID = "locale_division.address_id"; 
+	public static final String NAME = "locale.name";
+	public static final String LOCALE_STATE_NAME = "locale.state_name";
+	public static final String LOCALE_TYPE_ID = "locale.locale_type_id";
+	public static final String DIVISION_CODE = "division.division_code";
+	public static final String DIVISION_NBR = "division.division_nbr";
+	public static final String DIVISION_DISPLAY = "concat(division_nbr,'-',division_code)";
+	public static final String DESCRIPTION = "division.description"; 
+	public static final String ADDRESS1 = "address.address1";
+	public static final String ADDRESS2 = "address.address2";
+	public static final String CITY = "address.city";
+	public static final String STATE = "address.state";
+	public static final String ZIP = "address.zip";
 		
 	private Integer divisionId;
 	private Integer localeId;
