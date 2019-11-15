1b4c26d882a7c3e4441b7dd7a6b37b80091f71f2
Joshua Lewis <jwlewis@tardis2.thewebthing.com>
2019-11-15 16:04:42 -0600

    made SpecialOverrideResultSet

---
+++ src/com/ansi/scilla/web/specialOverride/common/SpecialOverrideResultSet.java
@@ -0,0 +1,12 @@
+package com.ansi.scilla.web.specialOverride.common;
+
+import com.ansi.scilla.report.reportBuilder.DataDumpReport;
+
+public class SpecialOverrideResultSet {
+	private SpecialOverrideType sot;
+	
+	public DataDumpReport ddr;
+	
+	
+	
+}
