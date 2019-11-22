5f0a6ed3ecb1734ee4adcee07980b9cb37e6c9d7
Joshua Lewis <jwlewis@tardis2.thewebthing.com>
2019-11-22 16:00:25 -0600

    web.xml, specialOverride.jsp, specialOverrideServlet

---
+++ src/com/ansi/scilla/web/specialOverride/servlet/SpecialOverrideServlet.java
@@ -0,0 +1,61 @@
+package com.ansi.scilla.web.specialOverride.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.ansi.scilla.web.common.response.ResponseCode;
+import com.ansi.scilla.web.common.response.WebMessages;
+import com.ansi.scilla.web.common.servlet.AbstractServlet;
+import com.ansi.scilla.web.common.utils.AnsiURL;
+import com.ansi.scilla.web.common.utils.AppUtils;
+import com.ansi.scilla.web.common.utils.Permission;
+import com.ansi.scilla.web.exceptions.ExpiredLoginException;
+import com.ansi.scilla.web.exceptions.NotAllowedException;
+import com.ansi.scilla.web.exceptions.ResourceNotFoundException;
+import com.ansi.scilla.web.exceptions.TimeoutException;
+import com.ansi.scilla.web.permission.response.PermissionListResponse;
+import com.thewebthing.commons.db2.RecordNotFoundException;
+
+public class SpecialOverrideServlet extends AbstractServlet {
+
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { //  Note : modeled after recommended uri parsing pattern 2018-04-19 kjw
+		AnsiURL url = null;
+		Connection conn = null;
+		WebMessages webMessages = new WebMessages();
+		try {
+			conn = AppUtils.getDBCPConn();
+			AppUtils.validateSession(request, Permission.PERMISSIONS_READ);
+
+			url = new AnsiURL(request, "permission", new String[] { ACTION_IS_LIST });	
+			if ( StringUtils.isBlank(url.getCommand() )) {
+				//validateGroupId(conn, url.getId());
+			}			
+			//PermissionListResponse permissionListResponse = makePermissionListResponse(conn, url);
+			webMessages.addMessage(WebMessages.GLOBAL_MESSAGE, "Success");										// add messages to the response
+			
+			//permissionListResponse.setWebMessages(webMessages);
+			
+			//super.sendResponse(conn, response, ResponseCode.SUCCESS, permissionListResponse);					// send the response
+		} catch (TimeoutException | NotAllowedException | ExpiredLoginException e) {							// these are thrown by session validation
+			super.sendForbidden(response);
+		} /*catch ( RecordNotFoundException e ) {			// if they're asking for an id that doesn't exist
+			super.sendNotFound(response);						
+		}*/ catch ( ResourceNotFoundException e) {		
+			super.sendNotFound(response);
+		} catch ( Exception e) {						// something bad happened
+			AppUtils.logException(e);
+			throw new ServletException(e);
+		} finally {
+			AppUtils.closeQuiet(conn);					// return the connection to the pool
+		}
+	}
+	
+}
