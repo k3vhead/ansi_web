69af3296b3ced29fa4cbef0229431d801813e606
Gary <gag@ansi.com>
2020-07-20 07:34:33 -0500

    Add more detail to the batch reports enum.

--- src/com/ansi/scilla/web/report/common/BatchReports.java
+++ src/com/ansi/scilla/web/report/common/BatchReports.java
@@ -2,35 +2,45 @@ package com.ansi.scilla.web.report.common;
 
 public enum BatchReports {
 
+	// Weekly
 	PAC_WEEKLY("PAC Weekly",                false, false, true),
-	
 	DO_LIST(   "Dispatched & Outstanding",  false, false, true),
 	PD45(      "45-Day Past Due",           false, false, true),
+
 	TSR(       "Ticket Status",             false, false, true),
+
+	// Monthly - before the month end
+	DRV(       "Detailed Rolling Volume",   false, false, true),
 	
 	
+	// Monthly - after the monthly close
 	PAC_MONTHLY("PAC Monthly",              false, false, true),
-	LIFT_GENIE( "Lift/Genie",               false, false, true),
+	LIFT_GENIE( "Lift/Genie Monthly",              true,  false, false),
+	CASH_RECEIPTS_REGISTER(
+			"Cash Receipts Register",       true,  false, false),
+	SERVICE_TAX("Service Taxes",            true,  false, false),
+	IR_SUMMARY( "Invoice Register Summary", true,  false, false),
+	IR(         "Invoice Register Detail",  false, false, true),
+	AR_TOTALS(  "Accounts Receivable w over 60 day detail", // divisions can get this but not batched
+											true,  false, false), 
+	AR_LISTING( "AR Listing",				true,  false, false), 	//exec (trend)
+
+	// Quarterly - after quarter close
 	PAC_QUARTER("PAC Quarterly",            false, false, true),
 	PAC_YTD(    "PAC Year-to-Date",         false, false, true),
 	SMRV(       "Six-Month Rolling Volume", false, false, true),
-	SKIPPED_AND_DISPATCHED(
-			"Skipped & Dispatched",         false, false, false),
-	
-	
-	
-	CASH_RECEIPTS_REGISTER(
-			"Cash Receipts Register",       true,  true,  false),
-	SERVICE_TAX("Service Tax",              false, false, true),
 	CREDIT_CARD_FEE_DISTRIBUTION(
-			"Credit Card Fee Distribution", false, false, false),
-	
-	
-	
-	IR_SUMMARY( "Invoice Register Summary", true,  true,  false),
-	IR(         "Invoice Register Detail",  false, false, true),
-	AR_TOTALS(  "Accounts Receivable",      true,  false, false),
-	WO_AND_FEES("WO & Fees",                false, false, false),
+			"Credit Card Fee Distribution", true,  false, false),
+	WO_AND_FEES("WO & Fees",                true,  false, false), 	// M, Q, Y?
+
+	// Executive reports - historical trending - generated quarterly
+	ACCOUNTS_RECEIVABLE( "AR w Claimed vs Billed",					// claimed/billed/cash
+											false, true,  true),
+	SKIPPED_AND_DISPATCHED( 										// lost volume for this and last quarter
+			"Skipped & Dispatched Counts",  false, true,  true),
+	QSS("Quarterly Sales Summary", 			true,  false, false),	// PAC trending
+	VOLUME_FORECAST("Volume Forecast",      false, true,  false),	// 6MRV trending
+	VPP("Volume Per Person",                false, true,  true),	// volume per paycheck paid out
 	;
 	
 	private String description;
