5a3a2cf969b4766548597e38b3b26f5077b6caed
Joshua Lewis <jwlewis@tardis2.thewebthing.com>
2021-02-10 11:12:19 -0600

    BcrTicketSpreadsheet: started creating the spreadsheet from sql

--- src/com/ansi/scilla/web/bcr/common/BcrTicketSpreadsheet.java
+++ src/com/ansi/scilla/web/bcr/common/BcrTicketSpreadsheet.java
@@ -1,7 +1,13 @@
 package com.ansi.scilla.web.bcr.common;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.Calendar;
 import java.util.List;
 
@@ -11,9 +17,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Level;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import com.ansi.scilla.report.reportBuilder.XLSBuilder;
 import com.ansi.scilla.web.bcr.servlet.AbstractBcrTicketLookupServlet;
 
 public class BcrTicketSpreadsheet extends AbstractBcrTicketLookupServlet{
@@ -26,10 +34,28 @@ public class BcrTicketSpreadsheet extends AbstractBcrTicketLookupServlet{
 		//this.BcrTicketSpreadsheet(conn, year, weeks);
 	}
 	
-	public BcrTicketSpreadsheet(Connection conn, Calendar date, List<String> workWeekList) {
+	public BcrTicketSpreadsheet(Connection conn, Calendar date, List<String> workWeekList) throws FileNotFoundException, IOException, SQLException {
 		String sql = BcrTicketSql.makeBaseWhereClause(workWeekList.get(0));
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		ResultSetMetaData rsmd = rs.getMetaData();
 		Integer year = date.YEAR;
-//		XSSFWorkbook workbook = XLSBuilder.build(report); 
+		XSSFWorkbook workbook = new XSSFWorkbook();
+		XSSFSheet sheet = workbook.createSheet();
+		Row row = null;
+		Cell cell = null;
+		workbook.setSheetName(0, "BCR Ticket Spreadsheet");
+		row = sheet.createRow(0);
+		int n = 0;
+		while(rs.next()) {
+			cell = row.createCell(n);
+			cell.setCellValue(rsmd.getColumnClassName(n));
+		}
+		
+		
+		
+		workbook.write(new FileOutputStream("/Users/dlewis/Documents/projects/pinpoint/google_cat/catdump_20131203.xlsx"));
+		
 	}
 	
 	@Override
