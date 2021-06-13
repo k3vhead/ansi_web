74776f93dced780d581dbb96f0ca92aad6b92302
Joshua Lewis <jwlewis@tardis2.thewebthing.com>
2021-06-13 10:53:41 -0500

    BCRTicketWorksheet, TestBCRWorksheet created

--- test/com/ansi/scilla/web/test/bcr/TestBcrWorksheet.java
+++ test/com/ansi/scilla/web/test/bcr/TestBcrWorksheet.java
@@ -7,12 +7,15 @@ import java.util.List;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import com.ansi.scilla.common.utils.AppUtils;
-import com.ansi.scilla.web.bcr.common.BCRSpreadsheet.BcrTicketSpreadsheet;
+import com.ansi.scilla.web.bcr.common.BCRSpreadsheet.BcrTicketWorksheet;
 import com.ansi.scilla.web.common.struts.SessionDivision;
 import com.ansi.scilla.web.test.TesterUtils;
 
-public class TestBcrSpreadsheet extends AbstractBcrTest {
-
+public class TestBcrWorksheet extends AbstractBcrTest {
+	/*
+	 * This is a test of a test BCR
+	 * 
+	 * */
 	public void go() throws Exception {
 		Connection conn = null;
 		try {
@@ -26,12 +29,12 @@ public class TestBcrSpreadsheet extends AbstractBcrTest {
 //			SessionUser sessionUser = sessionData.getUser();
 //			Integer userId = sessionUser.getUserId();
 			
-			BcrTicketSpreadsheet spreadsheet = new BcrTicketSpreadsheet(conn, userId, divisionList, div_12il02, claimYear, workWeekJune2021);
+			BcrTicketWorksheet spreadsheet = new BcrTicketWorksheet(conn, userId, divisionList, div_12il02, claimYear, workWeekJune2021);
 			XSSFWorkbook workbook = spreadsheet.getWorkbook();
 			if ( userId == USER_IS_DAVE) {
 				workbook.write(new FileOutputStream("/home/dclewis/Documents/webthing_v2/projects/ANSI/testresults/BCR_Spreadsheet.xlsx"));
 			} else if ( userId == USER_IS_JOSHUA ) {
-				workbook.write(new FileOutputStream("/home/jwlewis/Documents/projects/BCR_Spreadsheet.xlsx"));
+				workbook.write(new FileOutputStream("/home/jwlewis/Documents/projects/BCR_Worksheet.xlsx"));
 			} else {
 				throw new Exception("Where do I write the file?");
 			}
@@ -48,7 +51,7 @@ public class TestBcrSpreadsheet extends AbstractBcrTest {
 	
 	public static void main(String[] args) {
 		try {
-			new TestBcrSpreadsheet().go();
+			new TestBcrWorksheet().go();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
