69ff4020925b788bd2fafefc4205a774e8d0780f
Jessica Seaton <seatonj@tardis2.thewebthing.com>
2023-05-03 03:14:08 -0400

    Updates to add purchase order 	modified:   src/com/ansi/scilla/web/invoice/servlet/InvoiceDetailLookupServlet.java

--- src/com/ansi/scilla/web/invoice/servlet/InvoiceDetailLookupServlet.java
+++ src/com/ansi/scilla/web/invoice/servlet/InvoiceDetailLookupServlet.java
@@ -50,6 +50,7 @@ public class InvoiceDetailLookupServlet extends AbstractLookupServlet {
 				InvoiceDetailLookupQuery.TICKET_TYPE,
 				InvoiceDetailLookupQuery.TICKET_STATUS,
 				InvoiceDetailLookupQuery.COMPLETED_DATE,
+				InvoiceDetailLookupQuery.PURCHASE_ORDER,
 				InvoiceDetailLookupQuery.PPC,
 				InvoiceDetailLookupQuery.TAXES,
 				InvoiceDetailLookupQuery.TOTAL,
